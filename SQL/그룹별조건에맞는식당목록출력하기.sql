4979a1391475e61ae5fb8f8b50a64e91dbfd9def
박도연 <ehduszkdzkd@naver.com>
2024-10-18 16:00:06 +0900

    modify 그룹별조건에맞는식당목록출력하기.sql

--- SQL/그룹별조건에맞는식당목록출력하기.sql
+++ SQL/그룹별조건에맞는식당목록출력하기.sql
@@ -1,12 +1,17 @@
+-- 코드를 입력하세요
 SELECT member_name, review_text, DATE_FORMAT(review_date, '%Y-%m-%d') AS review_date
-FROM member_profile AS profile JOIN rest_review AS review
-ON profile.member_id = review.member_id
-WHERE profile.member_id = 
+FROM member_profile AS prof JOIN rest_review AS rev
+ON prof.member_id = rev.member_id
+WHERE prof.member_id =
 (
-SELECT member_id
-FROM rest_review
-GROUP BY member_id
-ORDER BY SUM(review_score) DESC
-LIMIT 1
+    SELECT member_id
+    FROM rest_review
+    GROUP BY member_id
+    HAVING SUM(review_score) = (
+        SELECT SUM(review_score) AS sum_score
+        FROM rest_review
+        GROUP BY member_id
+        ORDER BY sum_score DESC LIMIT 1
+    )
 )
 ORDER BY review_date, review_text
