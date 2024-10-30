dba72dea3fe13217065638f82a4731aeb421bbe5
박도연 <ehduszkdzkd@naver.com>
2024-10-30 20:49:12 +0900

    modify 특정조건을만족하는물고기별수와최대길이구하기.sql

--- SQL/특정조건을만족하는물고기별수와최대길이구하기.sql
+++ SQL/특정조건을만족하는물고기별수와최대길이구하기.sql
@@ -1,10 +1,5 @@
-SELECT COUNT(info.fish_type) AS fish_count, MAX(length) AS max_length, info.fish_type
-FROM fish_info AS info JOIN (
-    SELECT fish_type
-    FROM fish_info
-    GROUP BY fish_type
-    HAVING AVG(IFNULL(length, 10)) >= 33
-) AS tmp
-ON info.fish_type = tmp.fish_type
-GROUP BY info.fish_type
-ORDER BY info.fish_type
+SELECT COUNT(*) AS fish_count, MAX(IFNULL(length,10)) AS max_length, fish_type
+FROM fish_info
+GROUP BY fish_type
+HAVING AVG(IFNULL(length, 10)) >= 33
+ORDER BY fish_type
