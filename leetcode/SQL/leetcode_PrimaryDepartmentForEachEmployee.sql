b115442316ef50d8b410d9186172db6d7c0b6b07
DoYeonDo <ehduszkdzkd@naver.com>
2024-12-30 13:38:41 +0900

    update leetcode_PrimaryDepartmentForEachEmployee.sql

---
+++ leetcode/SQL/leetcode_PrimaryDepartmentForEachEmployee.sql
@@ -0,0 +1,14 @@
+(
+    SELECT employee_id, department_id
+    FROM employee
+    WHERE primary_flag = 'Y'
+    GROUP BY employee_id
+)
+UNION
+(
+    SELECT employee_id, department_id
+    FROM employee
+    GROUP BY employee_id
+    HAVING COUNT(employee_id)=1
+)
+ORDER BY employee_id
